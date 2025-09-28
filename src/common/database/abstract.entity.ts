5480d9a38e99fd58290b35678ffa738e2d9aecac
Yury <chernyakov.yuriy@gmail.com>
2025-09-28 00:16:19 -0400

    Add GraphQL ObjectType and ID field decorator to AbstractEntity

--- src/common/database/abstract.entity.ts
+++ src/common/database/abstract.entity.ts
@@ -1,8 +1,11 @@
 import { Schema, Prop } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @Schema()
+@ObjectType()
 export class AbstractEntity {
   @Prop({ type: SchemaTypes.ObjectId })
+  @Field(() => ID)
   _id: Types.ObjectId;
 }
