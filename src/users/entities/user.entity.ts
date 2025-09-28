afd7ec65e68627fea1c53ead6a5524bd5449a10c
Yury <chernyakov.yuriy@gmail.com>
2025-09-28 00:04:36 -0400

    Refactor database architecture to use entities instead of schemas, integrate User repository and service with new structure

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,15 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AbstractEntity } from '../../common/database/abstract.entity';
+import { Field } from '@nestjs/graphql';
 
-@ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Schema({ versionKey: false })
+export class User extends AbstractEntity {
+  @Prop()
+  @Field()
+  email: string;
+
+  @Prop()
+  password: string;
 }
+
+export const UserEntity = SchemaFactory.createForClass(User);
