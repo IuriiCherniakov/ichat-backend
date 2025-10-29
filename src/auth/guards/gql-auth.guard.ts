d1e12c3ba4c953617e18edbed6d8d37d90e86e63
Yury <chernyakov.yuriy@gmail.com>
2025-10-29 10:28:55 -0400

    Add GraphQL support for CurrentUser decorator and introduce GqlAuthGuard
    - Update `CurrentUser` decorator to handle both HTTP and GraphQL request types.
    - Create `GqlAuthGuard` to enable JWT authentication in GraphQL context.

---
+++ src/auth/guards/gql-auth.guard.ts
@@ -0,0 +1,10 @@
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export class GqlAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const gqlContext = GqlExecutionContext.create(context);
+    return gqlContext.getContext<GqlExecutionContext>();
+  }
+}
