c763cd03587f90a75711d82f55f90c46722ff24a
Yury <chernyakov.yuriy@gmail.com>
2025-10-31 12:22:55 -0400

    Use explicit type-only import for `Response` from `express` in `AuthController`.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, UseGuards, Res } from '@nestjs/common';
 import { CurrentUser } from './current-user.decorator';
-import express from 'express';
+import type { Response } from 'express';
 import { User } from '../users/entities/user.entity';
 import { LocalAuthGuard } from './guards/local-auth-guard';
 import { AuthService } from './auth.service';
@@ -13,9 +13,8 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
-    @Res({ passthrough: true }) response: express.Response, // TODO: fix it later
+    @Res({ passthrough: true }) response: Response,
   ) {
-    console.log('user from controller', user);
     return this.authService.login(user, response);
   }
 }
